c481ba1a38769e43b83836f81695677082eecb9e
Kasa95 <nachocasanova95@gmail.com>
2022-05-21 17:10:40 +0000

    v2 con botón de nueva carta

--- src/app.js
+++ src/app.js
@@ -2,19 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-// window.onload = function() {
-//   let palos = ["♦", "♥", "♠", "♣"];
-//   let randomPalo = palos[Math.floor(Math.random() * palos.length)];
+function shuffle() {
+  let palos = ["♦", "♥", "♠", "♣"];
+  let randomPalo = palos[Math.floor(Math.random() * palos.length)];
 
-//   let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-//   let randomNumero = numero[Math.floor(Math.random() * numero.length)];
+  let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let randomNumero = numero[Math.floor(Math.random() * numero.length)];
 
-//   if (randomPalo === "♥" || randomPalo === "♦") {
-//     document.getElementById("myCard").classList.add("redCard");
-//   } else {
-//     document.getElementById("myCard").classList.add("blackCard");
-//   }
-//   document.querySelector("#paloTop").innerHTML = randomPalo;
-//   document.querySelector("#paloBottom").innerHTML = randomPalo;
-//   document.querySelector(".numero").innerHTML = randomNumero;
-// };
+  document.getElementById("myCard").classList.remove("redCard");
+  document.getElementById("myCard").classList.remove("blackCard");
+
+  if (randomPalo === "♥" || randomPalo === "♦") {
+    document.getElementById("myCard").classList.add("redCard");
+  } else {
+    document.getElementById("myCard").classList.add("blackCard");
+  }
+  document.querySelector("#paloTop").innerHTML = randomPalo;
+  document.querySelector("#paloBottom").innerHTML = randomPalo;
+  document.querySelector(".numero").innerHTML = randomNumero;
+}
+
+window.onload = shuffle();
+
+document.getElementById("newCard").addEventListener("click", shuffle);
